435ef14d2549e59aefdfa78caf7abc94cca9d857
Ben Clark <bclark303@gmail.com>
2026-08-14 11:40:51 -0400

    Add Game Night sub-navigation

--- app/game-nights/layout.tsx
+++ app/game-nights/layout.tsx
@@ -1,6 +1,14 @@
 import type { ReactNode } from "react";
+
+import { GameNightSectionNav } from "@/components/GameNightSectionNav";
 import { LeagueWorkspaceNav } from "@/components/LeagueWorkspaceNav";
 
 export default function GameNightsLayout({ children }: { children: ReactNode }) {
-  return <><LeagueWorkspaceNav />{children}</>;
+  return (
+    <>
+      <LeagueWorkspaceNav />
+      <GameNightSectionNav />
+      {children}
+    </>
+  );
 }
