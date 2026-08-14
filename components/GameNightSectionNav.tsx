84eb14362d7ed11672c1e2a23426a0e1a193dfbb
Ben Clark <bclark303@gmail.com>
2026-08-14 11:43:25 -0400

    Add Control Room to Game Night navigation

--- components/GameNightSectionNav.tsx
+++ components/GameNightSectionNav.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 
 const sections = [
   ["/game-nights", "Hub"],
+  ["/game-nights/control", "Control Room"],
   ["/game-nights/setup", "Setup & Rules"],
   ["/game-nights/check-in", "Check-in"],
   ["/game-nights/teams", "Teams"],
